bf8fb8c763c382ab490fd2b31adb2690dcafe62d
contributor-1 <Contributor1@gmail.com>
2024-06-20 00:19:35 +0300

    feature-1-v1 commit

--- PrintsProject/src/Main.java
+++ PrintsProject/src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        printDoubles(7.6, 5.8);
     }
     public static void printDoubles(double a, double b) {
         System.out.println(a + b);
